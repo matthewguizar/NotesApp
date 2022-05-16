096742b1cf6fc1f23a5c60bea00e262aad39b493
Matthew Guizar <matthewrguizar@gmail.com>
2022-05-16 10:23:32 -0700

    finished day 1

---
+++ src/test/java/com/devmountain/noteAPP/NoteAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.devmountain.noteAPP;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NoteAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
