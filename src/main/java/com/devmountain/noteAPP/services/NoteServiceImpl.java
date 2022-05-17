24d16f06648b97d3caa11ecffb9a1146c65e2fdd
Matthew Guizar <matthewrguizar@gmail.com>
2022-05-17 12:33:01 -0700

    created service layer and added spring security framework

---
+++ src/main/java/com/devmountain/noteAPP/services/NoteServiceImpl.java
@@ -0,0 +1,4 @@
+package com.devmountain.noteAPP.services;
+
+public class NoteServiceImpl {
+}
---
+++ src/main/java/com/devmountain/noteAPP/services/UserServiceImpl.java
@@ -0,0 +1,11 @@
+package com.devmountain.noteAPP.services;
+
+import com.devmountain.noteAPP.repository.UserRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+@Service
+public class UserServiceImpl {
+    @Autowired
+    private UserRepository userRepository;
+}
